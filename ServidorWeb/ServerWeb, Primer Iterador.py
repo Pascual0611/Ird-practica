12b0eb3d085b1ea2021564cc813eed028f7caac6
Angel <angel.plandrove@udc.es>
2021-03-12 17:49:49 +0100

    segundo

--- ServidorWeb/ServerWeb, Primer Iterador.py
+++ ServidorWeb/ServerWeb, Primer Iterador.py
@@ -1,10 +1,12 @@
-#Primer Iterador
 #Servidor Web
 #navegador/Cliente utilizado: Firefox
 #Revisar el Open (Linea 47), User y Host (linea 38 y 39) no son necesarios
 import sys
 import socket
 import threading
+import datetime
+from datetime import datetime
+import os
 
 def main():
     if len(sys.argv) !=2:
@@ -31,7 +33,6 @@ def main():
                 a = mensaje.decode('UTF-8')
                 b = a.split(sep = '\n') #Ahora mi mensaje es una lista
                 c = b[0].split(' ')
-                print(c)
                 version = 'HTTP/1.1'
                 try:
                     metodo = c[0]
@@ -46,21 +47,54 @@ def main():
                     else:
                         pass
                     archivo = ('data/'+recurso)
-                    print(archivo)
                     print("{} /{} {}\n".format(metodo, recurso, version))
                     #Leer Archivo
-                    with open (archivo, 'r') as f:
-                        contenido = f.read()
+                    if archivo.endswith('.txt'):
+                        clase = 'text/plain'
+                        with open (archivo, 'r') as f:
+                            contenido = f.read()
+                    elif archivo.endswith('.html'):
+                        clase = 'text/html'
+                        with open (archivo, 'r') as f:
+                            contenido = f.read()
+                    elif archivo.endswith('.jpg'):
+                        clase = 'image/jpeg'
+                        with open (archivo, 'rb') as f:
+                            contenido = f.read()
+                    elif archivo.endswith('.gif'):
+                        clase = 'image/gif'
+                        with open (archivo, 'rb') as f:
+                            contenido = f.read()
+                    else:
+                        clase = 'application/octet-stream'
+                    tamano = str(os.path.getsize(archivo))
+                    mod = datetime.fromtimestamp(
+                        os.path.getmtime(archivo)).strftime("%a, %d %b %Y %H:%M:%S %Z")
                     clave = '200 Ok'
                 except (FileNotFoundError):
                     clave = '404 NOT FOUND'
                 except(PermissionError, IndexError, ZeroDivisionError):
                     clave = '400 BAD REQUEST'
                 finally:
-                    respuesta = ('{} {} \n\n'.format(version, clave))
+                    fecha = datetime.now()
+                    respuesta = ('{} {} \n'.format(version, clave))
                     x.send(respuesta.encode('UTF-8'))
                     if clave == '200 Ok':
-                        x.send(contenido.encode('UTF-8'))
+                        cabecera = ('Date {}\nServer localhost:5000\n').format(fecha)
+                        cabecera = cabecera + ('Content-Length {}\n').format(tamano)
+                        cabecera = cabecera + ('Content-Type {}\n').format(clase)
+                        cabecera = cabecera + ('Last-Modified {}\n\n').format(mod)
+                        x.send(cabecera.encode('UTF-8'))
+                        if metodo in ['GET']:
+                            try:
+                                if clase in ['text/plain', 'text/html']:
+                                    x.send(contenido.encode('UTF-8'))
+                                elif clase in ['image/jpeg', 'image/gif']:
+                                    x.send(contenido)
+                            except UnboundLocalError:
+                                pass
+                        else:
+                            pass
                     else:
                         pass
                 x.close()
